7a696ba462485b9a2e4fe08e0268024a5cc68923
Mariam Arabidze <marabidze@tbcbank.com.ge>
2020-10-03 18:29:51 +0400

    feat : add httpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './components/users/users.component';
 import { UserComponent } from './components/user/user.component';
+import { UsersService } from './services/users.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    UsersComponent,
-    UserComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, UsersComponent, UserComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [UsersService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

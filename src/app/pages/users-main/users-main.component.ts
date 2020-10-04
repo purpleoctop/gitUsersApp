d7e77b69fe113c1fbe93673dd21258da00c2a574
Mariam Arabidze <marabidze@tbcbank.com.ge>
2020-10-05 02:10:05 +0400

    feat : add last three searches

--- src/app/pages/users-main/users-main.component.ts
+++ src/app/pages/users-main/users-main.component.ts
@@ -1,11 +1,9 @@
-import { Component, OnChanges, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
-import { LocalStorage, StorageMap } from '@ngx-pwa/local-storage';
 import { forkJoin } from 'rxjs';
 import { map, mergeMap } from 'rxjs/operators';
 import { IUser } from 'src/app/models/IUser';
-import { SearchService } from 'src/app/services/search.service';
 import { UsersService } from 'src/app/services/users.service';
 
 @Component({
@@ -13,32 +11,19 @@ import { UsersService } from 'src/app/services/users.service';
   templateUrl: './users-main.component.html',
   styleUrls: ['./users-main.component.scss'],
 })
-export class UsersMainComponent implements OnInit, OnChanges {
+export class UsersMainComponent implements OnInit {
   searchForm: FormGroup;
   users: IUser[];
-  searchItems: string[];
+  searchItems: string[] = [];
   loaded = false;
 
-  _subscription_user_name;
   constructor(
     private fb: FormBuilder,
     private usersService: UsersService,
-    private router: Router,
-    private storage: StorageMap,
-    private searchService: SearchService
+    private router: Router
   ) {
-    this._subscription_user_name = this.searchService.searchitems.subscribe(
-      (value) => {
-        this.searchItems = value;
-        console.log(`this is value ${value}`);
-      }
-    );
-  }
-
-  ngOnChanges() {
-    console.log(this.users);
-    this.loaded = this.users.length > 0;
-    console.log(this.loaded);
+    let items = localStorage.getItem('items');
+    this.searchItems = JSON.parse(items) || [];
   }
 
   ngOnInit(): void {
@@ -47,18 +32,6 @@ export class UsersMainComponent implements OnInit, OnChanges {
     this.searchForm = this.fb.group({
       userName: [null],
     });
-    // this.storage.clear().subscribe(() => {});
-
-    // this.storage
-    //   .get('items'
-    //   , {
-    //     type: 'array',
-    //     items: { type: 'string' }
-    //    })
-    //   .subscribe((items) => {
-    //     console.log(items)
-    //      this.searchItems = items ? items : [];
-    //   });
   }
 
   search(userName) {
@@ -72,17 +45,14 @@ export class UsersMainComponent implements OnInit, OnChanges {
   }
 
   saveSearch(item) {
-    // this.searchItems.push(item);
-    this.searchService.saveItem(item);
-    // this.searchItems.push(item);
-    // this.searchItems.reverse();
-    // console.log(this.searchItems)
-    // if (this.searchItems.length > 3) {
-    //   this.searchItems.pop();
-    //   console.log(this.searchItems)
-    // }
-    // this.storage.set('items', this.searchItems).subscribe()
-    // this.storage.get('items').subscribe(item => console.log(`storage ${item}`))
+    if (this.searchItems.length >= 3) {
+      {
+        this.searchItems.reverse().pop();
+        this.searchItems.reverse();
+      }
+    }
+    this.searchItems.push(item);
+    localStorage.setItem('items', JSON.stringify(this.searchItems));
   }
 
   getfullUsers() {

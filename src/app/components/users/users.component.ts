a388852e275c95dbedadc3353110be41f253bc95
Mariam Arabidze <marabidze@tbcbank.com.ge>
2020-10-04 10:58:43 +0400

    feat : users component in progress

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { IUser } from 'src/app/models/IUser';
+import { UsersService } from 'src/app/services/users.service';
+import { map, mergeMap } from 'rxjs/operators';
+import { forkJoin } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  styleUrls: ['./users.component.scss'],
 })
 export class UsersComponent implements OnInit {
+  users: IUser[];
 
-  constructor() { }
+  constructor(private usersService: UsersService, private router: Router) {}
 
   ngOnInit(): void {
+    this.getUsers()
+      .pipe(
+        mergeMap((users) => {
+          const users$ = users.map((user) =>
+            this.getRepos(user.userName).pipe(
+              map((repos) => {
+                return { ...user, repos: repos.slice(0, 3) };
+              })
+            )
+          );
+          return forkJoin({ ...users$ });
+        })
+      )
+      .subscribe((users) => {
+        console.log(users);
+        this.users = Object.keys(users).map((key) => {
+          return users[key];
+        });
+      });
   }
 
+  getUsers() {
+    return this.usersService.getUsers();
+  }
+
+  getRepos(userName) {
+    return this.usersService.getUserRepos(userName);
+  }
 }

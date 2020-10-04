392cf42ac22d7d3785ed181d73697b8ed2e8fc43
Mariam Arabidze <marabidze@tbcbank.com.ge>
2020-10-04 11:51:04 +0400

    feat : add angular material icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UsersComponent } from './components/users/users.component';
 import { UserComponent } from './components/user/user.component';
 import { UsersService } from './services/users.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    // UsersComponent,
-    UserComponent
+  declarations: [AppComponent, UserComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatIconModule,
   ],
-  imports: [BrowserModule,  AppRoutingModule, HttpClientModule],
   providers: [UsersService],
   bootstrap: [AppComponent],
 })
--- src/app/components/users/users.module.ts
+++ src/app/components/users/users.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatIconModule } from '@angular/material/icon';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from '../users/users.component';
 
 @NgModule({
   declarations: [UsersComponent],
-  imports: [CommonModule, UsersRoutingModule],
+  imports: [CommonModule, UsersRoutingModule, MatIconModule],
 })
 export class UsersModule {}

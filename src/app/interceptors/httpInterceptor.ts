7d4aeb4ccc2f96fa0dc797649ea7ba025e564742
Mariam Arabidze <marabidze@tbcbank.com.ge>
2020-10-05 12:17:30 +0400

    fix : change http interceptor username

--- src/app/interceptors/httpInterceptor.ts
+++ src/app/interceptors/httpInterceptor.ts
@@ -13,8 +13,9 @@ export class HTTPInterceptor implements HttpInterceptor {
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
+    const user = 'purpleoctop'
     request = request.clone({
-      headers: request.headers.set('Authorization', 'Basic ' + 'purpleoctop'),
+      headers: request.headers.set('Authorization', `Basic ${user}` ),
     });
     return next.handle(request);
   }
